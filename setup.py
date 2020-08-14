3be48b0ac80815b276a413cc72a8d58367d9ec02
joe-fivefifty <joe@joe-fivefifty.com>
2020-08-14 13:57:33 +0200

    Initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    author='Joe Dean',
+    description='A small utility for managing script-specific settings and multiple configurations.',
+    download_url='https://github.com/joedeandev/SmallConfig/archive/smallconfig_v1.0.tar.gz',
+    license='unlicense',
+    name='smallconfig',
+    packages=['smallconfig'],
+    url='https://github.com/joedeandev/SmallConfig',
+    version='1.0',
+)
