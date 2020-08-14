3b23c58d997e946e547f29868c102273359ddea3
joe-fivefifty <joe@joe-fivefifty.com>
2020-08-14 14:05:52 +0200

    Updated setup.py to include the long description

--- setup.py
+++ setup.py
@@ -5,6 +5,8 @@ setup(
     description='A small utility for managing script-specific settings and multiple configurations.',
     download_url='https://github.com/joedeandev/SmallConfig/archive/smallconfig_v1.0.tar.gz',
     license='unlicense',
+    long_description=open('README.md', 'r').read(),
+    long_description_content_type='text/markdown',
     name='smallconfig',
     packages=['smallconfig'],
     url='https://github.com/joedeandev/SmallConfig',
